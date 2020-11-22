8b39536597605a8e26f34842bd87418811f20ce9
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-11-22 17:51:40 +0330

    Hero UiKit to sass

--- MrTab/Controllers/HomeController.cs
+++ MrTab/Controllers/HomeController.cs
@@ -12,5 +12,11 @@ namespace MrTab.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
     }
 }
---
+++ MrTab/ViewComponents/CroppieViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MrTab.ViewComponents
+{
+    [ViewComponent]
+    public class CroppieViewComponent : ViewComponent
+    {
+        public class MyClass
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public MyClass(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int width, int height)
+        {
+            MyClass a = new MyClass(width, height);
+            return await Task.FromResult((IViewComponentResult)View("Default", a));
+        }
+
+    }
+}
