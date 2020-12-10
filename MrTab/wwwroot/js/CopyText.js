d67cdd767cd2f8e66b95b0bed253df5e74a36118
Lemi <Lemi@DESKTOP-SDD69VQ>
2020-12-10 17:38:39 +0330

    Service worker and Routing

--- MrTab/wwwroot/js/CopyText.js
+++ MrTab/wwwroot/js/CopyText.js
@@ -1,4 +1,4 @@
-﻿function copy(text , message) {
+﻿function copy(text, message) {
     var textArea = document.createElement("textarea");
     textArea.value = text;
 
@@ -21,4 +21,67 @@
     if (message === undefined || message === null) return;
 
     UIkit.notification(message);
+}
+
+const isInStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const isIos = /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
+
+let deferredPrompt;
+const addButtons = document.getElementsByClassName('btn-pwa');
+window.addEventListener('beforeinstallprompt', (e) => {
+    try {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+
+        for (let addBtn of addButtons) {
+            if (isInStandaloneMode) {
+                addBtn.style.display = "none";
+            }
+            addBtn.addEventListener('click', btnDownloadClick);
+        }
+
+    } catch { }
+});
+
+
+function btnDownloadClick(e) {
+
+    if (isInStandaloneMode) {
+        UIkit.notification('برنامه نصب شده است');
+        return;
+    }
+    //Is not installed
+
+
+
+    if (isSafari && isIos) {
+        UIkit.modal(document.getElementById('Modal-PopupIos')).show();
+        return;
+    }
+
+    try {
+        deferredPrompt.prompt();
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
+}
+
+if (isSafari && isIos) {
+    setInterval(() => {
+        UIkit.modal(document.getElementById('Modal-PopupIos')).show();
+    }, 60000 * 2);
 }
--- MrTab/wwwroot/sw.js
+++ MrTab/wwwroot/sw.js
@@ -1,13 +1,10 @@
-﻿const staticCacheName = 'site-static-v0.0.1';
-const pagesCacheName = 'site-pages-v0.0.1';
+const staticCacheName = 'site-static-v0.0.1';
 const isOnline = navigator.onLine;
-const domain = self.origin;
 
 const assets = [
-
+    '/favicon.ico'
 ];
 
-const pages = ['/Test/GetRoutes', '/Test/Index', '/Entry/Login', '/Entry/Register', '/Entry/Validation', '/Entry/ForgotPassword', '/Entry/ChangePassword', '/Entry/CodeSent', '/Home/Index', '/Home/About', '/Home/Search', '/Home/Profile', '/Home/ToRestaurant', '/Restaurant/ViewSingle', '/User/Profile', '/RouteAnalyzer_Main/ShowAllRoutes']
 //install the service worker
 self.addEventListener('install', (evt) => {
     evt.waitUntil(
@@ -17,7 +14,6 @@ self.addEventListener('install', (evt) => {
             }
         })
     )
-
 });
 
 //activate service worker
@@ -32,38 +28,7 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-    //self.clients.matchAll().then(a => console.log(a));
-
-    let isPage = false;
-    for (let route of pages) {
-        if (evt.request.url.replace(domain, '').toLowerCase() === route.toLowerCase()) {
-            console.log(route);
-            isPage = true;
-            break;
-        }
-    }
-
-    //HERE IT IS
-    //if (isPage) {
-    //    if (isOnline) {
-    //        caches.match(evt.request).then((cacheRes) => {
-    //            return cacheRes || fetch(evt.request)
-    //                .then(fetchRes => {
-
-    //                    //if the url is a resource, cache it
-    //                    return caches.open(pagesCacheName).then(cache => {
-    //                        cache.put(evt.request.url, fetchRes.clone());
-
-    //                        return fetchRes;
-    //                    })
-
-    //                    return fetchRes;
-    //                })
-    //        })
-    //    }
-    //}
-
-    return;
+    if (!(evt.request.url.indexOf('http') === 0)) return;
 
     evt.respondWith(
         caches.match(evt.request)
@@ -72,9 +37,11 @@ self.addEventListener('fetch', (evt) => {
                     .then(fetchRes => {
 
                         //if the url is a resource, cache it
-                        if (evt.request.url.includes('/Styles/')
-                            || evt.request.url.includes('/Scripts/')
-                            || evt.request.url.includes('/Resources/')) {
+                        if (evt.request.url.includes('/css/')
+                            || evt.request.url.includes('/fonts/')
+                            || evt.request.url.includes('/js/')
+                            || evt.request.url.includes('/resources/'))
+                        {
 
                             return caches.open(staticCacheName).then(cache => {
                                 cache.put(evt.request.url, fetchRes.clone());
