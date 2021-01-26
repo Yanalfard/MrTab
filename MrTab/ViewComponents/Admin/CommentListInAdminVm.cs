24e4d9a69f391e3d7fb0739158229d70b34f101a
Mehdi Sahandi <mehdisahandi@gmail.com>
2021-01-26 14:08:22 +0330

    اضافه کردن نمایش پیام ها به ادمین

---
+++ MrTab/ViewComponents/Admin/CommentListInAdminVm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MrTab.ViewComponents.Admin
+{
+    public class CommentListInAdminVm : ViewComponent
+    {
+        private Core db = new Core();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("/Areas/Admin/Views/Comment/Components/CommentListInAdmin.cshtml", db.Comment.Get().OrderByDescending(i=>i.DateSubmited)));
+        }
+
+    }
+}
