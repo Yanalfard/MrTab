b54be86d093c2716e3bed62c24fa16f10480890b
Mehdi Sahandi <mehdisahandi@gmail.com>
2021-03-07 17:04:38 +0330

    category count

--- MrTab/ViewComponents/View/CategoryShowInHomeVm.cs
+++ MrTab/ViewComponents/View/CategoryShowInHomeVm.cs
@@ -12,7 +12,7 @@ namespace MrTab.ViewComponents.View
         private Core db = new Core();
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return await Task.FromResult((IViewComponentResult)View("/Views/Shared/Components/CategoryShowInHomeVm/CategoryShowInHome.cshtml", db.Catagory.Get().Where(i=>i.IsHome && i.TblRestaurant.Count != 0).OrderByDescending(i=>i.CatagoryId).Skip(1)));
+            return await Task.FromResult((IViewComponentResult)View("/Views/Shared/Components/CategoryShowInHomeVm/CategoryShowInHome.cshtml", db.Catagory.Get().Where(i=>i.IsHome && i.TblRestaurant.Count != 0).OrderByDescending(i=>i.TblRestaurant.Count()).Skip(1)));
         }
     }
 }
